cf828012280e5ce38e42715b0e7f598200ae6fe7
Ian McCurry <ianmccurry4@gmail.com>
2023-10-06 11:15:38 -0700

    Linked front and back ends

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -1,5 +1,7 @@
 // backend.js
 import express from "express";
+import cors from 'cors';
+
 
 const users = { 
     users_list :
@@ -35,6 +37,7 @@ const users = {
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,8 +1,11 @@
 // src/MyApp.js
-import React, {useState} from 'react';
 import Table from './Table';
 // src/MyApp.js (Adding the form component after the table)
 import Form from './Form';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
+
   
 function MyApp() {
   
@@ -18,6 +21,27 @@ function MyApp() {
 	    });
 	  setCharacters(updated);
 	}
+
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:8000/users');
+      return response.data.users_list;     
+    }
+    catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+    }
+  }
+
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+        setCharacters(result);
+    });
+  }, [] );
+ 
+
   return (
     <div className="container">
       <Table characterData={characters} 
