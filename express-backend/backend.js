a0fd1ab4c013e11dc06f9dbaeebfe9cc1d60679f
Ian McCurry <ianmccurry4@gmail.com>
2023-10-06 10:49:45 -0700

    Added Delete and Get with jobs

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -43,18 +43,34 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
+    const job = req.query.job;
+    if (name != undefined && job != undefined){
+        let result = findUserByNameAndJob(name, job);
+        result = {users_list: result};
+        res.send(result);
+    }else if(name != undefined && job == undefined){
         let result = findUserByName(name);
         result = {users_list: result};
         res.send(result);
-    }
-    else{
+    }else if(name == undefined && job != undefined){
+        let result = findUserByJob(job);
+        result = {users_list: result};
+        res.send(result);
+    }else{
         res.send(users);
     }
 });
 
 const findUserByName = (name) => { 
     return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByJob = (job) => { 
+    return users['users_list'].filter( (user) => user['job'] === job); 
+}
+
+const findUserByNameAndJob = (name, job) => { 
+    return users['users_list'].filter( (user) => user['name'] === name && user['job'] === job); 
 }
 
 app.get('/users/:id', (req, res) => {
@@ -71,6 +87,31 @@ app.get('/users/:id', (req, res) => {
 function findUserById(id) {
     return users['users_list'].find( (user) => user['id'] === id); // or line below
     //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id'];
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        deleteUser(result);
+        res.status(204).end();
+    }
+})
+
+function deleteUser(user){
+    users['users_list'].splice(users['users_list'].indexOf(user),1);
 }
 
 app.listen(port, () => {
